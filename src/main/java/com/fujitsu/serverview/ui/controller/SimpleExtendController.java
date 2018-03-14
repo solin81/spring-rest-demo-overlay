b86b0f5876feabf81c20eb6f35e0cc2b04681034
Maciej Solinski <Maciej.Solinski@ts.fujitsu.com>
2018-03-14 16:39:46 +0100

    Initial commit

---
+++ src/main/java/com/fujitsu/serverview/ui/controller/SimpleExtendController.java
@@ -0,0 +1,15 @@
+package com.fujitsu.serverview.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Maciej Solinski <a href="mailto:maciej.solinski@ts.fujitsu.com">maciej.solinski@ts.fujitsu.com</a>
+ */
+@RestController
+@RequestMapping("/SimpleExtendController")
+public class SimpleExtendController {
+
+
+
+}
---
+++ src/main/webapp/welcome.jsp
@@ -0,0 +1,1 @@
+Welcome from spring-rest-demo-overlay
